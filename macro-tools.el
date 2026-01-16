400edfee347f855453a254462220bd90d2625906
John Grey <johngrey4296@gmail.com>
2026-01-16 14:54:47 +0000

    [chore]:

--- macro-tools.el
+++ macro-tools.el
@@ -24,9 +24,9 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-(require 'macro-tools--hydra)
-(require 'macro-tools--related)
-(require 'macro-tools--transient)
+;; (require 'macro-tools--hydra)
+;; (require 'macro-tools--related)
+;; (require 'macro-tools--transient)
 (require 'macro-tools--util)
 
 (provide 'macro-tools)
